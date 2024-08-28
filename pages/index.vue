cd86aa9349cf534ef62b3b2ae6fd62dab18b8f6f
Uchchhas <msruchchhas.dev@gmail.com>
2024-08-28 12:09:02 +0600

    Test commit

--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,7 @@
 <template>
     <div class="grid place-items-center min-h-screen">
         <div>
-            <button @click="setLocale('en')">en</button>
+            <button @click="setLocale('en')" class="hello">en</button>
             <button @click="setLocale('ro')">ro</button>
             <p>{{ $t('welcome') }}</p>
         </div>
