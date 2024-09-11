4f29c45e6d9eb586dc6505d5b04e71409b6a0b3b
Uchchhas <msruchchhas.dev@gmail.com>
2024-09-11 19:06:43 +0600

    About page WIP

---
+++ pages/about/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+    import AboutPageSectionOne from '~/components/features/about/AboutPageSectionOne.vue'
+    import AboutPageSectionTwo from '~/components/features/about/AboutPageSectionTwo.vue'
+    import AboutPageSectionThree from '~/components/features/about/AboutPageSectionThree.vue'
+    import AboutPageSectionFour from '~/components/features/about/AboutPageSectionFour.vue'
+</script>
+
+<template>
+    <about-page-section-one/>
+    <about-page-section-two/>
+    <about-page-section-three/>
+    <about-page-section-four/>
+</template>
---
+++ pages/contact/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+    import ContactPageSectionOne from '~/components/features/contact/ContactPageSectionOne.vue'
+    import ContactPageSectionTwo from '~/components/features/contact/ContactPageSectionTwo.vue'
+    import ContactPageSectionThree from '~/components/features/contact/ContactPageSectionThree.vue'
+    import ContactPageSectionFour from '~/components/features/contact/ContactPageSectionFour.vue'
+</script>
+
+<template>
+    <contact-page-section-one/>
+    <contact-page-section-two/>
+    <contact-page-section-three/>
+    <contact-page-section-four/>
+</template>
