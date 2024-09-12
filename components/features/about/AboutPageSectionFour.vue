2f2791a92006b1743f5ef243b571b64f28bbdf84
Uchchhas <msruchchhas.dev@gmail.com>
2024-09-12 10:21:07 +0600

    About page WIP

--- components/features/about/AboutPageSectionFour.vue
+++ components/features/about/AboutPageSectionFour.vue
@@ -1,7 +1,28 @@
 <script setup lang="ts">
-
+    import {teams} from '~/config'
+    import AppBreadcrumb from '~/components/shared/AppBreadcrumb.vue'
 </script>
 
 <template>
-
+    <section class="py-10 laptop:py-[96px] bg-gray-50">
+        <div class="container">
+            <app-breadcrumb
+                class="mb-8 laptop:mb-16 max-w-[768px]"
+                title="Meet our team"
+                description="Our team is composed of passionate professionals with extensive experience in agriculture, technology, and business development. Key members include"
+            />
+            <div class="grid laptop:grid-cols-3 gap-8">
+                <div
+                    v-for="(team, teamIndex) in teams"
+                    class="flex flex-col items-center justify-center gap-5"
+                >
+                    <NuxtImg width="80" height="80" :src="team.avatar" :alt="team.name" />
+                    <div>
+                        <p class="text-gray-900 font-medium text-b2">{{ team.name }}</p>
+                        <p class="text-primary-700">{{ team.designation }}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 </template>
---
+++ config/teams.ts
@@ -0,0 +1,38 @@
+export const teams = [
+    {
+        id: 1,
+        name: 'Abu Naim Ahmad',
+        designation: 'Founder & CEO',
+        avatar: '/images/teams/team-image-1.png',
+    },
+    {
+        id: 2,
+        name: 'Mihaela Dobirceanu',
+        designation: 'Co-founder & BusDev',
+        avatar: '/images/teams/team-image-2.png',
+    },
+    {
+        id: 3,
+        name: 'Alina Chiteala',
+        designation: 'Co-founder & COO',
+        avatar: '/images/teams/team-image-3.png',
+    },
+    {
+        id: 4,
+        name: 'Ghaith Hamadeh',
+        designation: 'Co-founder & CFO',
+        avatar: '/images/teams/team-image-4.png',
+    },
+    {
+        id: 5,
+        name: 'Abu Naim Ahmad',
+        designation: 'Co-founder & CSO',
+        avatar: '/images/teams/team-image-5.png',
+    },
+    {
+        id: 6,
+        name: 'Robert Constantinescu',
+        designation: 'Co-founder & CMO',
+        avatar: '/images/teams/team-image-6.png',
+    },
+]
