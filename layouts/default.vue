c618f4d75832a106ed9df852264fc5127e6184be
Uchchhas <hello.devstrail@gmail.com>
2025-02-14 15:24:38 +0600

    Local changes

--- layouts/default.vue
+++ layouts/default.vue
@@ -8,7 +8,7 @@
 </script>
 
 <template>
-    <!--<div class="bg-white h-screen flex flex-col items-center justify-center">
+    <div class="bg-white h-screen flex flex-col items-center justify-center">
         <div class="text-center">
             <h1 class="text-4xl md:text-5xl font-bold text-gray-900 mb-4">
                 504 Gateway Time-out
@@ -16,11 +16,11 @@
             <p class="text-lg text-gray-500">nginx</p>
         </div>
         <hr class="w-full border-t border-gray-300 mt-10" />
-    </div>-->
-    <div class="prose tablet:prose-tablet laptop:prose-laptop desktop:prose-desktop min-h-screen flex flex-col pt-[72px] laptop:pt-[96px]">
+    </div>
+    <!--<div class="prose tablet:prose-tablet laptop:prose-laptop desktop:prose-desktop min-h-screen flex flex-col pt-[72px] laptop:pt-[96px]">
         <AppCommonLayoutHeader/>
         <slot/>
         <AppCommonLayoutFooter class="mt-auto"/>
         <app-auth-alert-modal v-model:isOpen="authStore.isAuthAlertModalOpen" />
-    </div>
+    </div>-->
 </template>
